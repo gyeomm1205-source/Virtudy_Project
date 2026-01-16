da4132ae285d86669b9a8bbd63708183eaddf0b9
김희원 <kimhiwon@naver.com>
2026-01-16 13:28:34 +0900

    BE: init setting

---
+++ BE/src/main/java/com/ssafy/virtudy/VirtudyApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.virtudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtudyApplication.class, args);
+	}
+
+}
