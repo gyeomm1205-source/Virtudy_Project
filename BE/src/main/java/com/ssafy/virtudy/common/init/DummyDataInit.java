20e75986e1d2bbed81bb8cfdcb8b001dc0735beb
김희원 <kimhiwon@naver.com>
2026-01-21 15:47:33 +0900

    fix: 약관 동의 필드 변경; 3개로 나눔

--- BE/src/main/java/com/ssafy/virtudy/common/init/DummyDataInit.java
+++ BE/src/main/java/com/ssafy/virtudy/common/init/DummyDataInit.java
@@ -103,7 +103,9 @@ public class DummyDataInit implements CommandLineRunner {
                 .memberId("user" + index)
                 .password("{noop}1234") // 테스트용 암호화 안 된 비번
                 .email(faker.internet().emailAddress())
-                .isAgreed(AgreedType.VIDEO)
+                .isVideoAgreed(true)
+                .isServiceAgreed(true)
+                .isPersonalAgreed(true)
                 .nickName(faker.name().fullName()) // "김철수", "이영희" 등 생성
                 .jobType(JobType.values()[random.nextInt(JobType.values().length)]) // Enum 랜덤
                 .status(MemberStatType.ACTIVE)
