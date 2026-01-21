5d57f129b72852a82b83e7c8a26dd9adff8499eb
김희원 <kimhiwon@naver.com>
2026-01-21 15:46:55 +0900

    add: 카카오 로그인 이후, 필수로 입력해야 하는 (회원가입) 사용자 정보 추가

--- BE/src/main/java/com/ssafy/virtudy/member/dto/MemberSignUpRequest.java
+++ BE/src/main/java/com/ssafy/virtudy/member/dto/MemberSignUpRequest.java
@@ -17,6 +17,16 @@ public class MemberSignUpRequest {
     @NotBlank
     private String nickname;   // 사용자가 수정한 닉네임
 
+    // 약관 동의 여부 (프론트에서 true/false로 옴)
+    // @AssertTrue를 붙이면 false가 들어올 때 아예 요청을 튕겨낼 수도 있음 (선택사항)
+    @NotNull
+    private Boolean isServiceAgreed;
+
+    @NotNull
+    private Boolean isVideoAgreed;
+
+    @NotNull
+    private Boolean isPersonaAgreed;
     /**
      * 학습 성향 입력 받기 위한 곳
      * 1. 1일 평균 공부 시간
@@ -27,9 +37,12 @@ public class MemberSignUpRequest {
     @NotNull
     private StudyType studyType;  // "MARATHON", "SPRINTER" 등 (Enum 추천하지만 일단 String)
 
+    @NotNull
     private ActiveTimeType activeTimeType; // 새벽/ 오전/ 오후/ 저녁
 
+    @NotNull
     private JobType jobType;
 
+    @NotNull
     private int targetHours; // 목표 시간
 }
