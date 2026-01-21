765b794505d2e34b0d1e8a5cebffbb012c0a437c
길지운 <lak2577@naver.com>
2026-01-21 15:13:57 +0900

    [S14P11A703-104-ru] 사용자 정보 RU

--- BE/src/main/java/com/ssafy/virtudy/member/domain/Member.java
+++ BE/src/main/java/com/ssafy/virtudy/member/domain/Member.java
@@ -50,6 +50,7 @@ public class Member extends BaseTimeEntity {
 
     // Member.java 내부에 필드 추가
     @Column(nullable = false)
+    @Builder.Default
     private Integer avatarGenCount = 0; // 아바타 생성 횟수 (기본값 0)
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -58,5 +59,10 @@ public class Member extends BaseTimeEntity {
 
     public void setFavoriteRoom(StudyRoom studyRoom) {
         this.favoriteRoom = studyRoom;
+    }
+
+    public void updateProfile(String nickName, JobType jobType) {
+        this.nickName = nickName;
+        this.jobType = jobType;
     }
 }
