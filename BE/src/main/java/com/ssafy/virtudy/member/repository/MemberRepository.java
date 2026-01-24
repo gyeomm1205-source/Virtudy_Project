48327cddde0aed1caf69294574aec1ae88f93c8e
totodle <daiswhat@gmail.com>
2026-01-24 21:44:10 +0900

    feature: 랭킹 서비스 추가, 팀ë ë(개인, 팀) 랭킹 조회, (개인팀) 랭킹 ê ²색, ì상위 5명 랭킹\
    조회, (자신의 랭킹 혹은 최애 팀 랭킹 조회 완료, 자정 랭킹 갱신 스케줄러 등록 완료 \
    더미 데이터 오ë¥ 수정 및 최신화 그리고 ko -> en 으로 변경. \
    StudyRoomRepository : title로 검색 추가, MemberRepository : nickname으로 검색 추가

--- BE/src/main/java/com/ssafy/virtudy/member/repository/MemberRepository.java
+++ BE/src/main/java/com/ssafy/virtudy/member/repository/MemberRepository.java
@@ -4,12 +4,14 @@ import com.ssafy.virtudy.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByMemberId(String memberId);
 
+    List<Member> findByNickName(String nickName);
     Optional<Member> findByEmail(String email);
 
     boolean existsByMemberId(String memberId);
