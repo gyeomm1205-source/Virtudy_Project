467da118e32afafc3dcd0a8a78619e0dc5099916
totodle <daiswhat@gmail.com>
2026-02-02 13:22:00 +0900

    fix(ai):update the url fastapi

--- AI/create_avatar/app/main.py
+++ AI/create_avatar/app/main.py
@@ -28,7 +28,7 @@ app.add_middleware(
 
 # 3. 라우터 등록
 # 이제 /makeAvatar 같은 API 주소를 사용할 수 있게 됩니다.
-app.include_router(avatar.router)
+app.include_router(avatar.router, prefix="/fastapi")
 
 # 4. (선택사항) 서버가 잘 켜졌는지 확인하는 테스트용 루트 경로
 @app.get("/")
