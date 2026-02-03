b707f244e3ea1040bae1f277ac72870daa7f5dd0
길지운 <lak2577@naver.com>
2026-02-03 17:00:21 +0900

    [S14P11A703-261] 사용자 학습 패턴 주기적 최신화

--- BE/src/main/java/com/ssafy/virtudy/study/service/StudySessionService.java
+++ BE/src/main/java/com/ssafy/virtudy/study/service/StudySessionService.java
@@ -50,11 +50,7 @@ public class StudySessionService {
 
         // [Fix] Ghost Session Logic: 기존 세션이 있다면 강제 종료 후 재입장 허용
         studySessionRepository.findByMemberAndEndTimeIsNull(member)
-                .ifPresent(
-                studySession -> {
-                    studySession.close(0);
-                }
-        );
+                .ifPresent(StudySession::close);
 
         int currentUsers = studySessionRepository.findByRoomAndEndTimeIsNull(room).size();
         if (currentUsers >= MAX_USER) {
@@ -130,14 +126,14 @@ public class StudySessionService {
         return enterRoom(member, selectedRoom.getRoomId());
     }
 
-    public void exitRoom(String memberId, int sessionRealStudyTime) {
+    public void exitRoom(String memberId) {
         Member member = memberRepository.findByMemberId(memberId)
                 .orElseThrow(() -> new BaseException(BaseErrorCode.MEMBER_NOT_FOUND_ERROR));
 
         StudySession session = studySessionRepository.findByMemberAndEndTimeIsNull(member)
                 .orElseThrow(() -> new BaseException(BaseErrorCode.ROOM_NOT_PARTICIPATE_ERROR));
 
-        session.close(sessionRealStudyTime);
+        session.close();
     }
 
     private double calculateRoomDistance(MemberPreference userPref, MemberGameStat userStat, StudyRoom room, List<Member> roomMembers) {
